5e4b796457dd021d97971a3e255d97afa505b4f5
LillyCode <leannelaceybyrne@outlook.com>
2024-02-14 08:46:26 +0000

    creating and using theme in material & react
    Signed-off-by: LillyCode <leannelaceybyrne@outlook.com>

---
+++ src/Theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme ({
+    palette: {
+        primary: {
+            main: '#686868',
+        },
+        slateBlue: {
+            main: '#777FEB',
+        }
+    },
+})
+
+export default theme;
