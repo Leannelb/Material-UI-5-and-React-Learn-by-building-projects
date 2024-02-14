ffc6af89b0453958e739250c9bb310318e62be06
LillyCode <leannelaceybyrne@outlook.com>
2024-02-14 09:29:45 +0000

    adding breakpoints to theme - materialui
    Signed-off-by: LillyCode <leannelaceybyrne@outlook.com>

--- src/Theme.js
+++ src/Theme.js
@@ -9,6 +9,15 @@ export const theme = createTheme ({
             main: '#777FEB',
         }
     },
+    typography:{
+        h2: {
+            fontFamily: 'Monserrat, sans-serif',
+            fontWeight: 700,
+            fontSize: '32px',
+            lineHeight: '39px',
+            color: '#000000'
+        }
+    }
 })
 
 export default theme;
