44d25d91c1a379746b5e6624d25952b2919f120b
LillyCode <leannelaceybyrne@outlook.com>
2024-03-12 08:15:33 +0000

    adding color from theme

--- src/Theme.js
+++ src/Theme.js
@@ -24,6 +24,17 @@ export const theme = createTheme({
         lineHeight: '78px'
       }
     },
+    h3: {
+      fontFamily: 'Monserrat, sans-serif',
+      fontWeight: 700,
+      fontSize: '24px',
+      lineHeight: '29px',
+      color: '#000000',
+      [breakpoints.up('md')]: {
+        fontSize: '64px',
+        lineHeight: '78px'
+      }
+    },
     body1: {
       fontFamily: 'Monserrat, sans-serif',
       fontWeight: 400,
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -3,13 +3,25 @@ const styles = {
         textAlign: 'left',
         borderRadius: '8px',
         border: `0.5px solid, ${theme.palette.slateBlue.main}`,
+        //adding color from theme 
         padding: '44px',
         width: '403px',
         boxSizing: 'border-box',
         [theme.breakpoints.up('md')]: {
-            maxWidth:'403px'
+            maxWidth: '403px'
         }
-    })
+    }),
+    btn: {
+        //adding color from theme 
+        color: theme.palette.slateBlue.main,
+        fontFamily: 'Montserrat',
+        fontSize: '16px',
+        fontStyle: 'normal',
+        fontWeight: '500',
+        lineHeight: 'normal',
+        textDecorationLine: 'underline',
+        textTransform: 'capitalize',
+    }
 }
 
 export default styles;
