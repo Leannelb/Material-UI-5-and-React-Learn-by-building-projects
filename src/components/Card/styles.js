b1692e7e041ad4a86fdc278d1b184206cdb3bc11
LillyCode <leannelaceybyrne@outlook.com>
2024-03-12 07:57:16 +0000

    adding a color from theme to local css file

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,5 +1,15 @@
 const styles = {
-
+    card: (theme) => ({
+        textAlign: 'left',
+        borderRadius: '8px',
+        border: `0.5px solid, ${theme.palette.slateBlue.main}`,
+        padding: '44px',
+        width: '403px',
+        boxSizing: 'border-box',
+        [theme.breakpoints.up('md')]: {
+            maxWidth:'403px'
+        }
+    })
 }
 
 export default styles;
